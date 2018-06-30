d1538d4c7b57712ab0398f8fd968cdd6a5c0f711
vocabexpresshaxer <amar13s@royallatin.org>
2018-06-30 21:57:17 +0100

    Update discordmain.py

--- discordmain.py
+++ discordmain.py
@@ -15,7 +15,10 @@ async def on_message(message):
     if message.content in ("status", "Status", "status?", "Status?"):
         msg = 'The Bot Is Currently Active'
         await client.send_message(message.channel, msg)
-
+    elif message.conten.lower() == "help":
+        helpmsg = "Bot made by @CaffieneAddict420#8646\nUsage:\n*+life [phone number]*\n*+verify [code] [referal code]*"
+        await client.send_message(message.channel, helpmsg)
+    
     
     elif message.content.startswith('+life'):
         lifeargs = message.content.split(" ")
