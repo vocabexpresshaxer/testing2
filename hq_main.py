65800e9a1ec3757219a48214043a83ca2fda0801
vocabexpresshaxer <amar13s@royallatin.org>
2018-07-01 20:10:37 +0100

    Update hq_main.py

--- hq_main.py
+++ hq_main.py
@@ -133,7 +133,7 @@ def getResponse(data):
                 try:
                     auth = extralives.create_user(uname, ver, ref, "GB")['authToken']
                     bearers.append(str(auth))
-                    test = HQClient(auth) ################
+                    test = extralives.HQClient(auth) ################
                     return(str(auth) + "\n" + str(test.me())) #################
                     pickle.dump(bearers, open("/root/bearers.p", "wb"))
                     return("Life is Queued For Creation During Next US Game")
