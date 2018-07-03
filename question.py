d7db446f25bd6e9d247abb465c8964e1b306fd77
vocabexpresshaxer <amar13s@royallatin.org>
2018-07-03 08:09:17 +0100

    Update question.py

--- question.py
+++ question.py
@@ -67,7 +67,6 @@ async def answer_question(question, original_answers):
     if inaccurate == True:
         toWrite = toWrite + "*Bot Highly Likely To Pick Wrong Answer For This Question Due To Answer Being In Question*\n"
     if best_answer == "":
-        #best_answer = await __search_method2(search_text, answers, reverse)
         toWrite = toWrite + "\nMethod 1: *inconclusive*"
         
         best_answer, points = await __search_method2(search_text, answers, reverse)
