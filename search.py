c95dc1bb6f897d1f01df60987b0783058ffc3c43
vocabexpresshaxer <amar13s@royallatin.org>
2018-07-05 21:08:28 +0100

    Update search.py

--- search.py
+++ search.py
@@ -103,7 +103,7 @@ async def search_google(question, num_results, method = 1):
 async def multiple_search(questions, num_results):
     queries = list(map(GOOGLE_URL.format, questions))
     pages = await networking.get_responses(queries, timeout=5, headers=HEADERS)
-    link_list = [get_google_links(page, num_results, 2) for page in pages]
+    link_list = [get_google_links(page, num_results) for page in pages]
     return link_list
 
 
