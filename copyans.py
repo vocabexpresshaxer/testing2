c78b2b270f2db3017be611447b52ceb9a8a09665
vocabexpresshaxer <amar13s@royallatin.org>
2018-07-09 17:52:58 +0100

    Update copyans.py

--- copyans.py
+++ copyans.py
@@ -9,10 +9,13 @@ def obfuscate(byt):
     return bytes(c ^ mask[i % lmask] for i, c in enumerate(byt))
 
 def on_message():
+    global last
     while True:
         chatid = "463094183272644640"
         message = client.get_message(chatid)
-        client.send_message("465899313403527169", message)
+        if message != last:
+            client.send_message("465899313403527169", message)
+            last = message
 
 def getChoice(m1, m2):
     choice1 = random.choice(("1", "2", "3", "4"))
@@ -38,6 +41,7 @@ def getChoicev2(m):
         return random.choice(("1", "2", "3"))
 
 if __name__ == '__main__':
+    last = ""
     stupid = obfuscate(b'%!4\r"\x18![+=2\x15?\x1e.P63"\n*?0\x00Y+\x1a\x0f_ \x0eY\x1a\x08"<<\x17\x1b9\x1d4*"\x1e\x1e\x01"1\x01,7<\x07\x136[3I').decode()
     client = zenon.Client(stupid)
     client.func_loop(on_message)
