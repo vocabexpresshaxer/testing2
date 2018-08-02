6fb415ec818dce1ee6d818399d0543695fbff259
vocabexpresshaxer <amar13s@royallatin.org>
2018-08-02 22:54:41 +0100

    Update discordmain.py

--- discordmain.py
+++ discordmain.py
@@ -95,6 +95,9 @@ Usage:
             for b in totalb:
                 botuser = extralives.HQClient(b)
                 bot_payout_info = botuser.payouts()
+                if region == "us" and bot_bal.startswith("£"):
+                    totalb.remove(b)
+                    #pickle.dumps(totalb, 
                 bot_bal = bot_payout_info.balance
                 print(str(index) + " : " + str(bot_bal.unpaid))
                 await client.send_message(message.channel, str(index) + " : " + str(bot_bal.unpaid))
