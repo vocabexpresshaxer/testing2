ce0a75f32288ce2930e4b12b7a56c17cb1d7a10b
vocabexpresshaxer <amar13s@royallatin.org>
2018-08-16 10:43:05 +0100

    Update gambling.py

--- gambling.py
+++ gambling.py
@@ -1,32 +1,34 @@
-import re, json, random, time, discord, pickle
-import asyncio
-from _thread import start_new_thread
+import re, json, random, time, zenon, pickle
 
 def obfuscate(byt):
     mask = b'keyword'
     lmask = len(mask)
     return bytes(c ^ mask[i % lmask] for i, c in enumerate(byt))
 
-client = discord.Client()
-stupid = obfuscate(b'%!4\r"\x18![+=2\x15?\x1e.P63"\n*?0\x00Y+\x1a\x0f_ \x0eY\x1a\x08"<<\x17\x1b9\x1d4*"\x1e\x1e\x01"1\x01,7<\x07\x136[3I').decode()
-client.run(stupid)
 
-@client.event
-async def on_ready():
-    global auth
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+def on_message():
+    global last
+    while True:
+        chatid = "462330902580822037"
+        message = client.get_message(chatid)
+        try:
+            message = message.split("\"value\": \"")[1]
+            message = message.split("\"}, {\"")[0]
+            message = message.replace("\\u2705", "")
+
+            #print(message)
+            if message != last:
+                print(message)
+                last = message
+        except:
+            pass
 
 
-@client.event
-async def on_message(message):
-    global ukNo
-    global auth
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+if __name__ == '__main__':
+    last = ""
+    stupid = obfuscate(b'%!4\r"\x18![+=2\x15?\x1e.P63"\n*?0\x00Y+\x1a\x0f_ \x0eY\x1a\x08"<<\x17\x1b9\x1d4*"\x1e\x1e\x01"1\x01,7<\x07\x136[3I').decode()
+    client = zenon.Client(stupid)
+    client.func_loop(on_message)
     
     
 
