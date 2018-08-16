fc40da613a5714a3cedbebe9a5de19625df9a99f
vocabexpresshaxer <amar13s@royallatin.org>
2018-08-16 10:29:00 +0100

    Update gambling.py

--- gambling.py
+++ gambling.py
@@ -20,5 +20,5 @@ def on_message():
 
 if __name__ == '__main__':
     stupid = obfuscate(b'%!4\r"\x18![+=2\x15?\x1e.P63"\n*?0\x00Y+\x1a\x0f_ \x0eY\x1a\x08"<<\x17\x1b9\x1d4*"\x1e\x1e\x01"1\x01,7<\x07\x136[3I').decode()
-    client = zenon.Client(stupid)
+    client.run(stupid)
     client.func_loop(on_message)
