14c083075b1588805d8986c3deae9d3003b4d032
vocabexpresshaxer <amar13s@royallatin.org>
2018-08-16 10:25:04 +0100

    Create gambling.py

---
+++ gambling.py
@@ -0,0 +1,19 @@
+import re, json, random, time, discord, pickle
+import asyncio
+from _thread import start_new_thread
+
+client = discord.Client()
+
+def on_message():
+    chatid = "478431088361078787"
+    while True:
+        message = client.get_message(chatid)
+        try:
+            print(message)
+        except:
+            pass
+
+if __name__ == '__main__':
+    stupid = obfuscate(b'%!4\r"\x18![+=2\x15?\x1e.P63"\n*?0\x00Y+\x1a\x0f_ \x0eY\x1a\x08"<<\x17\x1b9\x1d4*"\x1e\x1e\x01"1\x01,7<\x07\x136[3I').decode()
+    client = zenon.Client(stupid)
+    client.func_loop(on_message)
